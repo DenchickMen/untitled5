a42cd6d5b604cc91663740e9cbf3cdd175f71f10
student <gudenis234@gmail.com>
2022-11-12 17:49:11 +0200

    Initial commit

---
+++ fthxf.py
@@ -0,0 +1,20 @@
+class Child:
+    speed = 10
+    height = 25
+    def __init__(self):
+        print(self.heidgt)
+        print(self.speed)
+
+
+colorama = Child()
+import requests
+help(requests)
+rq = requests
+print('for den', requests.__cake__)
+print(rq.__name__)
+print(rq.__url__)
+print(type(3))
+ls = []
+for i in dir(ls):
+    print(i)
+print(dir(ls))
